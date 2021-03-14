6129a124cde3670f974b789628aef9333ac9e909
Naveed Shahzad <lhrciit@gmail.com>
2021-03-14 14:55:11 +0500

    push livewire default code

---
+++ resources/views/pages/home_bootstrap.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.bootstrap')
+
+@section('content')
+<div class="col-md-8">
+    <div class="card">
+        <div class="card-header">
+            <h3>Introduction: Bootstrap Layout</h3>
+        </div>
+        <div class="card-body">
+
+      
+        </div>
+    </div>
+</div>
+@endsection
